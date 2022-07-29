c1bced7ef0a2ed37839397369a9cffa0ba5f0b6a
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-07-28 20:18:27 -0400

    starting up auth messarounds

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
+  import { onAuthStateChanged } from "firebase/auth";
+  import { onMount } from "svelte";
   import Actions from "./components/Actions.svelte";
   import DatePicker from "./components/DatePicker.svelte";
   import Day from "./components/Day.svelte";
+  import { auth } from "./lib/firebase";
   import { days } from "./stores/days";
+
+  onMount(() => {
+    onAuthStateChanged(
+      auth,
+      (user) => {
+        console.log(user);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  });
 </script>
 
 <main>
