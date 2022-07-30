cf1482ca6c1344fb2cf37821baf95637dd86b419
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-07-30 09:14:48 -0400

    updating entry adds to use common modal component

--- src/components/PeeAdd.svelte
+++ src/components/PeeAdd.svelte
@@ -1,59 +1,15 @@
 <script lang="ts">
-  import { fade } from "svelte/transition";
   import { addPee, type PeeAmount } from "../stores/pees";
+  import EntryModal from "./EntryModal.svelte";
   import PeeAmountInput from "./PeeAmountInput.svelte";
 
-  let open = false;
   let amount: PeeAmount = 2;
 
-  const setOpen = () => {
-    open = true;
-  };
-
-  const setClosed = () => {
-    open = false;
-  };
+  const onAdd = () => addPee({ amount });
 </script>
 
-<button on:click={setOpen}>💧</button>
-
-{#if open}
-  <aside on:click={setClosed} transition:fade={{ duration: 100 }}>
-    <section on:click|stopPropagation>
-      <article>
-        <PeeAmountInput bind:amount />
-      </article>
-
-      <button
-        on:click={() => {
-          addPee({ amount });
-          setClosed();
-        }}
-      >
-        add
-      </button>
-    </section>
-  </aside>
-{/if}
-
-<style>
-  aside {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    z-index: 100;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  section {
-    color: #000;
-    background: #fff;
-    padding: 0.5rem;
-  }
-</style>
+<EntryModal icon="💧" okText="add" onOk={onAdd}>
+  <article>
+    <PeeAmountInput bind:amount />
+  </article>
+</EntryModal>
