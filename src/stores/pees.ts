514c1e99bfc86bf4fb414438b474aa2bb3016c32
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-07-31 14:53:37 -0400

    removing unused

--- src/stores/pees.ts
+++ src/stores/pees.ts
@@ -1,5 +1,5 @@
-import { EntryBase } from "$lib/entry";
 import { firestore } from "$lib/firebase";
+import { Pee, PeeAdd } from "$models/pees";
 import {
   collection,
   deleteDoc,
@@ -14,7 +14,6 @@ import {
   type Timestamp,
 } from "firebase/firestore";
 import { derived, get } from "svelte/store";
-import { Pee, PeeAdd } from "$models/pees";
 import { user } from "./user";
 
 const getPeesCollection = (uid: string) =>
