6175d9b4277148f23034a20b2d6243fbbc2c782b
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-07-31 15:33:31 -0400

    adding animations to entries as they enter/leave

--- src/components/Entry.svelte
+++ src/components/Entry.svelte
@@ -1,10 +1,12 @@
 <script lang="ts" context="module">
   import type { Entry, EntryKind } from "$models/entries";
   import type { ComponentType } from "svelte";
+  import { slide } from "svelte/transition";
   import FeedUpdate from "./FeedUpdate.svelte";
   import NapUpdate from "./NapUpdate.svelte";
   import PeeUpdate from "./PeeUpdate.svelte";
   import PoopUpdate from "./PoopUpdate.svelte";
+
   // used to dynamically render the correct component
   const components: {
     [K in EntryKind]: ComponentType;
@@ -22,7 +24,7 @@
   let component = components[entry.kind];
 </script>
 
-<div>
+<div transition:slide>
   <span>{entry.timestamp.toLocaleTimeString()}</span>
 
   {#if component}
