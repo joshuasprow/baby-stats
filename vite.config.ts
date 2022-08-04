f2e5beb2b6181dbb0dfad5be311c88bdd708f1da
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-04 19:10:54 -0400

    making config a little more custom

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 
+const dir = (...parts: string[]) => resolve(__dirname, ...parts);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   resolve: {
     alias: {
-      $components: resolve(__dirname, "src/components"),
-      $lib: resolve(__dirname, "src/lib"),
-      $models: resolve(__dirname, "src/models"),
-      $stores: resolve(__dirname, "src/stores"),
+      $components: dir("src", "components"),
+      $lib: dir("src", "lib"),
+      $models: dir("src", "models"),
+      $stores: dir("src", "stores"),
     },
   },
 });
