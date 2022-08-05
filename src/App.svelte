4b7a624c85076897f4d5b45a602f36fd655c7676
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-05 06:07:44 -0400

    moving app styles to css file per svelte warning

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,6 @@
 <script lang="ts">
   import Layout from "$components/Layout/Layout.svelte";
+  import "./main.css";
 </script>
 
 <Layout />
-
-<style global>
-  :root {
-    font-family: sans-serif;
-    font-size: 24px;
-    line-height: 32px;
-    font-weight: 400;
-
-    font-synthesis: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-text-size-adjust: 100%;
-
-    --action-bar-color: rgba(255, 255, 255, 0.7);
-    --action-bar-height: 54px;
-  }
-
-  body {
-    margin: 0;
-  }
-
-  button,
-  input,
-  select {
-    font-size: 1rem;
-  }
-</style>
