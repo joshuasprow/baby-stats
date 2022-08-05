02bee428998ca807d3d5aa734d6bf98e44491774
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-05 06:11:57 -0400

    adding page title

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import Layout from "$components/Layout/Layout.svelte";
+  // TODO: add baby name to title
+  // import { user } from "$stores/user";
   import "./main.css";
 </script>
 
+<svelte:head>
+  <title>baby stats</title>
+</svelte:head>
 <Layout />
