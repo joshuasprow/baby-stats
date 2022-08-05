c5106789f3b22e726fe56be543cae0d3efe7abc8
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-04 22:53:28 -0400

    making layout component folder

---
+++ src/components/Layout/Footer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import FeedAdd from "$components/Feed/FeedAdd.svelte";
+  import NapAdd from "$components/Nap/NapAdd.svelte";
+  import PeeAdd from "$components/Pee/PeeAdd.svelte";
+  import PoopAdd from "$components/Poop/PoopAdd.svelte";
+</script>
+
+<footer>
+  <FeedAdd />
+  <NapAdd />
+  <PeeAdd />
+  <PoopAdd />
+</footer>
+
+<style>
+  footer {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    bottom: env(safe-area-inset-bottom);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: var(--action-bar-height);
+    padding: 0 0.25rem;
+    background: var(--action-bar-color);
+  }
+</style>
---
+++ src/components/Layout/Layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Entries from "$components/Entries.svelte";
+  import Footer from "$components/Layout/Footer.svelte";
+  import Header from "$components/Layout/Header.svelte";
+  import SignInButton from "$components/SignInButton.svelte";
+  import { days } from "$stores/days";
+  import { user } from "$stores/user";
+</script>
+
+<!-- firebase hasn't loaded yet -->
+{#if $user === undefined}
+  <main class="centered">
+    <span>⏳</span>
+  </main>
+  <!-- user is signed out -->
+{:else if $user === null}
+  <main class="centered">
+    <span>Please sign in below</span>
+    <SignInButton />
+  </main>
+{:else if $days.length === 0}
+  <main class="centered">
+    <span>Use the buttons below to add new entries</span>
+  </main>
+{:else}
+  <main>
+    <Entries />
+  </main>
+
+  <Header />
+  <Footer />
+{/if}
+
+<style>
+  main {
+    padding: var(--action-bar-height) 0.5rem;
+  }
+
+  .centered {
+    position: absolute;
+    inset: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
