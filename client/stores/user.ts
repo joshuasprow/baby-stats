870ef7ba7036be6ec393ec1d56e7145f0f291815
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-05 19:29:24 -0400

    adding data validation for user

--- client/stores/user.ts
+++ client/stores/user.ts
@@ -4,24 +4,110 @@ import {
   onAuthStateChanged,
   signInWithPopup,
   signOut as _signOut,
-  type User,
+  type User as AuthUser,
+  type UserInfo,
 } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { readable } from "svelte/store";
+import { z } from "zod";
+
+const ProviderData = z.object({
+  providerId: z.string(),
+  uid: z.string(),
+  displayName: z.string().nullable(),
+  email: z.string().email().nullable(),
+  phoneNumber: z.string().nullable(),
+  photoURL: z.string().url().nullable(),
+});
+type ProviderData = z.infer<typeof ProviderData>;
+
+const User = z.object({
+  uid: z.string(),
+  email: z.string().email().nullable(),
+  emailVerified: z.boolean(),
+  displayName: z.string(),
+  isAnonymous: z.boolean(),
+  photoURL: z.string().url().nullable(),
+  providerData: z.array(ProviderData),
+  createdAt: z.date(),
+  lastLoginAt: z.date(),
+});
+export type User = z.infer<typeof User>;
+
+const parseProviderData = (providerData: UserInfo): ProviderData => ({
+  providerId: providerData.providerId,
+  uid: providerData.uid,
+  displayName: providerData.displayName,
+  email: providerData.email,
+  phoneNumber: providerData.phoneNumber,
+  photoURL: providerData.photoURL,
+});
+
+const hasProperty = (obj: unknown, key: string) =>
+  Object.prototype.hasOwnProperty.call(obj, key);
+
+const dateFromJson = (string: unknown) => {
+  if (typeof string !== "string") {
+    throw new Error(`Expected string, got ${string} ${typeof string}`);
+  }
+
+  const unix = parseInt(string, 10);
+
+  if (isNaN(unix)) {
+    throw new Error(`Expected number, got ${string}`);
+  }
+
+  return new Date(unix);
+};
+
+const parseUser = (_user: AuthUser) => {
+  const json = _user.toJSON();
+
+  if (!hasProperty(json, "createdAt")) {
+    throw new Error("User has no createdAt");
+  }
+  if (!hasProperty(json, "lastLoginAt")) {
+    throw new Error("User has no lastLoginAt");
+  }
+  if (!hasProperty(json, "email")) {
+    throw new Error("User has no email");
+  }
+
+  const createdAt = dateFromJson((json as any).createdAt);
+  const lastLoginAt = dateFromJson((json as any).lastLoginAt);
+
+  const email = z
+    .string()
+    .nullable()
+    .parse((json as any).email);
+
+  return User.parse({
+    uid: _user.uid,
+    email,
+    emailVerified: _user.emailVerified,
+    displayName: _user.displayName,
+    isAnonymous: _user.isAnonymous,
+    photoURL: _user.photoURL,
+    providerData: _user.providerData.map(parseProviderData),
+    createdAt,
+    lastLoginAt,
+  });
+};
 
 const getUserDoc = (uid: string) => doc(firestore, `users/${uid}`);
 
-const updateUserDoc = async (_user: User) => {
+const updateUserDoc = async (_user: AuthUser) => {
   try {
     const ref = getUserDoc(_user.uid);
+    const parsed = parseUser(_user);
 
-    await setDoc(ref, user);
+    await setDoc(ref, parsed);
   } catch (error) {
     console.error(error);
   }
 };
 
-export const user = readable<User | null | undefined>(undefined, (set) => {
+export const user = readable<AuthUser | null | undefined>(undefined, (set) => {
   const unsubscribe = onAuthStateChanged(auth, async (_user) => {
     set(_user);
 
