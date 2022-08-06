29088b2f9c2785b55b3fefef9270d3c0019841cc
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-05 22:00:52 -0400

    removing eslint... will replace after figuring out how paths work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,9 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+export const takeSnapshot = functions.firestore
+  .document("/users/{userId}/feeds/{feedId}")
+  .onWrite(async (change, context) => {
+    functions.logger.log("feed write", { data: change.after.data() });
+  });
