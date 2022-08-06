f58b914958ed699fed346db1bc0b32c72879f9df
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-05 21:49:24 -0400

    adding model for snapshots that should be taken on doc writes

---
+++ models/snapshots.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { Feed } from "./feeds";
+import { Nap } from "./naps";
+import { Pee } from "./pees";
+import { Poop } from "./poops";
+
+export const Snapshot = z.object({
+  id: z.string(),
+  createdAt: z.date(),
+  uid: z.string(),
+  feeds: z.array(Feed),
+  naps: z.array(Nap),
+  pees: z.array(Pee).nonempty(),
+  poops: z.array(Poop).nonempty(),
+});
+export type Snapshot = z.infer<typeof Snapshot>;
