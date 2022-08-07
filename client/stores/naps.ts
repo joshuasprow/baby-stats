7f05e2e80ad575d82a68237e8bf63d7245ceddad
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-07 17:14:56 -0400

    updating nap records when fetching if they have invalid amounts

--- client/stores/naps.ts
+++ client/stores/naps.ts
@@ -25,27 +25,35 @@ const getNapDoc = (uid: string, id: string) =>
 
 // TODO: remove this after migrating all nap records
 const fixOldNapAmount = (
-  amount: number | TimeRangeAmount,
+  amount: number | { start: Timestamp; end: Timestamp },
   timestamp: Date
-): TimeRangeAmount => {
-  if (typeof amount !== "number") return amount;
+): [amount: TimeRangeAmount, fixed: boolean] => {
+  if (typeof amount !== "number") {
+    return [{ start: amount.start.toDate(), end: amount.end.toDate() }, false];
+  }
 
   const start = new Date(timestamp);
   const end = new Date(timestamp);
 
   end.setMinutes(start.getMinutes() + amount * 15 /* 15 minutes per "unit" */);
 
-  return { start, end };
+  return [{ start, end }, true];
 };
+
+// TODO: remove this after migrating all nap records
+let NAP_FIX_QUEUE: NapNext[] = [];
 
 const napFromDoc = (doc: QueryDocumentSnapshot<DocumentData>): NapNext => {
   const data = doc.data();
   const timestamp = (data.timestamp as Timestamp).toDate();
 
-  const amount = fixOldNapAmount(data.amount, timestamp);
+  const [amount, fixed] = fixOldNapAmount(data.amount, timestamp);
 
-  console.log(amount);
   const nap = NapNext.parse({ ...data, amount, id: doc.id, timestamp });
+
+  if (fixed) {
+    NAP_FIX_QUEUE.push(nap);
+  }
 
   return nap;
 };
@@ -61,12 +69,34 @@ export const naps = derived<typeof user, (Nap | NapNext)[]>(
       return unsubscribe;
     }
 
+    let updating = false;
+
     unsubscribe = onSnapshot(
       query(getNapsCollection($user.uid), orderBy("timestamp", "desc")),
       (snap) => {
+        NAP_FIX_QUEUE = [];
+
         const $naps = snap.docs.map(napFromDoc);
 
         set($naps);
+
+        if (NAP_FIX_QUEUE.length === 0) return;
+        if (updating) {
+          console.log("naps already updating");
+          return;
+        }
+
+        console.log(`updating ${NAP_FIX_QUEUE.length} fixed naps`);
+        updating = true;
+
+        Promise.all(NAP_FIX_QUEUE.map(updateNap))
+          .catch((error) => {
+            console.error(error);
+          })
+          .finally(() => {
+            updating = false;
+            console.log(`updated  ${NAP_FIX_QUEUE.length} fixed naps`);
+          });
       }
     );
 
