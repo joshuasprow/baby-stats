90628177a613f883e976fd9645fee49f4cedf5e5
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-10 21:14:34 -0400

    renaming, adding type export to firebase package

---
+++ models/time.ts
@@ -0,0 +1,19 @@
+import { Timestamp } from "baby-stats-firebase/types";
+import { z } from "zod";
+
+export const newTimestamp = (date = new Date()) =>
+  new Timestamp(date.getSeconds(), 0);
+
+export const TimeRangeAmount = z
+  .object({ start: z.instanceof(Timestamp), end: z.instanceof(Timestamp) })
+  .refine((s) => s.start <= s.end, "start must be before end");
+export type TimeRangeAmount = z.infer<typeof TimeRangeAmount>;
+
+export const isTimeRangeAmount = (value: unknown) => {
+  try {
+    TimeRangeAmount.parse(value);
+    return true;
+  } catch {
+    return false;
+  }
+};
