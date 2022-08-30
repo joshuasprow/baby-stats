e867cd7b580bf677d190fa3e58f92f4ae2a640cb
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-29 22:08:16 -0400

    making an emoji icon with a solid text shadow

---
+++ client/components/Emoji.svelte
@@ -0,0 +1,23 @@
+<i class="emoji"><slot /></i>
+
+<style>
+  .emoji {
+    --x: 0.75;
+    --bw: calc(var(--x) * var(--border-width));
+    --bw-neg: calc(-1 * var(--x) * var(--border-width));
+
+    /* 🔥 FULL COVERAGE OVERKILL! 🔥 */
+    text-shadow: 0 0 0 var(--border-color), 0 0 var(--bw) var(--border-color),
+      0 0 var(--bw-neg) var(--border-color),
+      0 var(--bw) var(--bw) var(--border-color),
+      0 var(--bw) var(--bw-neg) var(--border-color),
+      0 var(--bw-neg) var(--bw) var(--border-color),
+      0 var(--bw-neg) var(--bw-neg) var(--border-color),
+      var(--bw) var(--bw) var(--bw) var(--border-color),
+      var(--bw) var(--bw) var(--bw-neg) var(--border-color),
+      var(--bw) var(--bw-neg) var(--bw-neg) var(--border-color),
+      var(--bw-neg) var(--bw-neg) var(--bw-neg) var(--border-color),
+      var(--bw-neg) var(--bw-neg) var(--bw) var(--border-color),
+      var(--bw-neg) var(--bw) var(--bw) var(--border-color);
+  }
+</style>
