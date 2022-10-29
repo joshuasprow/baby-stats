84f7767ee257ecf7dd39d0ccadb8f6c60c419325
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-10-28 20:29:26 -0400

    removing now-unused models in client folder

--- client/models/babies.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const Baby = z.object({
-  id: z.string().min(1),
-  name: z.string().optional(),
-  parents: z.array(z.string().min(1)),
-});
-export type Baby = z.infer<typeof Baby>;
