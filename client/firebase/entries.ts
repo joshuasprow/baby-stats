ea391eb21419238636d70fd503f99af1dad59321
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-01 11:14:03 -0400

    refactoring

--- client/firebase/entries.ts
+++ client/firebase/entries.ts
@@ -6,41 +6,44 @@ import { Poop } from "@baby-stats/models/poops";
 import {
   collection,
   onSnapshot,
+  orderBy,
   query,
   where,
+  type DocumentData,
   type Firestore,
 } from "@firebase/firestore";
 import { addDays } from "../lib/dates";
 
 const getEntriesCollection = (db: Firestore, babyId: string) =>
-  query(
-    collection(db, "entries"),
-    where("babyId", "==", babyId),
-    where("timestamp", ">=", addDays(new Date(), -10)),
-  );
+  query(collection(db, "entries"), where("babyId", "==", babyId));
+
+const parseEntryData = (data: DocumentData): Entry<EntryKind> => {
+  switch (data.kind as EntryKind) {
+    case "feeds":
+      return Feed.parse(data);
+    case "naps":
+      return Nap.parse(data);
+    case "pees":
+      return Pee.parse(data);
+    case "poops":
+      return Poop.parse(data);
+    default:
+      throw new Error(`Unknown entry kind: ${data.kind}`);
+  }
+};
 
 export const subscribeToEntries = (
   db: Firestore,
   babyId: string,
   set: (entries: Entry<EntryKind>[]) => void,
 ) =>
-  onSnapshot(getEntriesCollection(db, babyId), (snap) =>
-    set(
-      snap.docs.map((doc) => {
-        const data = doc.data();
-
-        switch (data.kind as EntryKind) {
-          case "feeds":
-            return Feed.parse(data);
-          case "naps":
-            return Nap.parse(data);
-          case "pees":
-            return Pee.parse(data);
-          case "poops":
-            return Poop.parse(data);
-          default:
-            throw new Error(`Unknown entry kind: ${data.kind}`);
-        }
-      }),
+  onSnapshot(
+    query(
+      getEntriesCollection(db, babyId),
+      where("timestamp", ">=", addDays(new Date(), -10)),
+      orderBy("timestamp", "desc"),
     ),
+    { includeMetadataChanges: true },
+    (snap) => set(snap.docs.map((doc) => parseEntryData(doc.data()))),
+    (error) => console.error(error),
   );
