b71dd8de4f7188624a488060df043e1eccb0c195
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-20 16:30:34 -0500

    adding and configuring vite-plugin-pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,38 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 const dir = (...parts: string[]) => resolve(__dirname, ...parts);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Baby Stats",
+        short_name: "BabyStats",
+        description: "For keeping track of baby.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: dir("dist"),
   },
