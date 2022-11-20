9ddf211d453b444b0491ee3e4f6f49122835a626
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-20 16:41:16 -0500

    enabling firestore persistence

--- client/firebase/app.ts
+++ client/firebase/app.ts
@@ -1,10 +1,15 @@
 import { initializeApp as initializeFirebaseApp } from "@firebase/app";
 import { getAuth } from "@firebase/auth";
-import { getFirestore } from "@firebase/firestore";
+import { enableIndexedDbPersistence, getFirestore } from "@firebase/firestore";
 import type { FirebaseOptions } from "./options";
 
 export const initializeApp = (options: FirebaseOptions) => {
   const app = initializeFirebaseApp(options);
 
-  return { auth: getAuth(app), db: getFirestore(app) };
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  enableIndexedDbPersistence(db);
+
+  return { auth, db };
 };
