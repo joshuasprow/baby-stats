bda753b8cd8900175009571ffe84f0da9b1dc84e
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-20 19:11:20 -0500

    refrencing client types and adding callbacks for service worker events

--- client/main.ts
+++ client/main.ts
@@ -1,4 +1,21 @@
 import App from "./App.svelte";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+    console.log("sw: need refresh");
+  },
+  onOfflineReady() {
+    console.log("sf: offline ready");
+  },
+  onRegisterError(error) {
+    console.log("sw: register error: ", error);
+  },
+  onRegisteredSW(swScriptUrl, registration) {
+    console.log("sw: registered: ", swScriptUrl, registration);
+  },
+});
 
 const target = document.getElementById("app");
 
