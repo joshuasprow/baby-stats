5cf90416a8aaccdf9db6787004cb92b0d24d65ea
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-27 12:23:41 -0500

    usinig esbuild and firebase functions codebase in order to bundle models package before deployment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,31 @@
-import { onRequest } from "firebase-functions/v2/https";
+import { Log } from "@baby-stats/models/logs";
 import { logger } from "firebase-functions/v2";
+import { onRequest } from "firebase-functions/v2/https";
+import { ZodError } from "zod";
+
+const parseError = (error: unknown, nameIfUnknown = "Unknown"): Error => {
+  if (!error) return new Error("Empty error");
+
+  if (error instanceof Error) return error;
+
+  if (typeof error === "string") return new Error(error);
+
+  const unknown = new Error(`${nameIfUnknown}: ${JSON.stringify(error)}`);
+  unknown.name = nameIfUnknown;
+
+  return unknown;
+};
+
+const formatZodError = (error: ZodError): string => {
+  const errors = Object.entries(error.flatten().fieldErrors);
+  const prefix = "Invalid Log Entry -- Field Errors:";
+
+  return errors.reduce((acc, [key, value]) => {
+    const message = `${key}: ${value?.join(", ")}`;
+
+    return [acc, message].join("; ");
+  }, prefix);
+};
 
 export const logs = onRequest(
   {
@@ -18,7 +44,29 @@ export const logs = onRequest(
       return;
     }
 
-    logger.info("Hello logs!", { structuredData: true });
+    let entry: Log;
+
+    try {
+      entry = Log.parse(request.body);
+    } catch (e) {
+      const error = parseError(e, "ParseError");
+
+      logger.error(error);
+
+      if (error instanceof ZodError) {
+        const formatted = formatZodError(error);
+        response.status(400).send(formatted);
+        return;
+      }
+
+      response.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const { message, ...rest } = entry;
+
+    logger.info(message, rest);
+
     response.send("Hello from Firebase!");
   }
 );
