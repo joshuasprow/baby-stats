f460add2e6f5516d0824844bd7b4eb2b6197c72b
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-30 21:22:25 -0500

    handling escape correctly in more components. making uncaught error modal closable

--- client/components/Layout/Error.svelte
+++ client/components/Layout/Error.svelte
@@ -1,29 +1,45 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import logger from "../../lib/logger";
   import { parseError } from "@baby-stats/lib/error";
+  import { onMount } from "svelte";
+  import { pressEscape } from "../../lib/actions";
+  import logger from "../../lib/logger";
+  import Button from "../Button.svelte";
 
   let error: Error | null = null;
+  let open = true;
+
+  const reload = () => {
+    window.location.reload();
+  };
+
+  const close = () => {
+    open = false;
+  };
 
   onMount(() => {
     window.onunhandledrejection = (e) => {
+      console.log(e);
       error = parseError(e.reason);
       logger.error(error);
     };
   });
 </script>
 
-{#if error}
-  <section class="error-backdrop">
-    <article class="error-block">
-      <p class="error">
+{#if error && open}
+  <!-- svelte-ignore a11y-click-events-have-key-events *pressEscape action -->
+  <div class="error-backdrop" on:click={close} use:pressEscape={close}>
+    <aside class="error-block">
+      <p>
         {error.message}
       </p>
-      <p class="error">
-        You'll need to reload the page... maybe try again later?
-      </p>
-    </article>
-  </section>
+      <p>You'll need to reload the page... maybe try again later?</p>
+
+      <footer>
+        <Button on:click={reload}>reload</Button>
+        <Button on:click={close}>cancel</Button>
+      </footer>
+    </aside>
+  </div>
 {/if}
 
 <style>
@@ -47,8 +63,9 @@
     padding: 1rem;
   }
 
-  .error {
-    background-color: var(--color-error);
-    color: var(--color-error-text);
+  footer {
+    display: flex;
+    justify-content: flex-end;
+    gap: 0.5rem;
   }
 </style>
