7c0b61bd1776c72bfdce9bc99311ba2831a79090
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-30 20:13:00 -0500

    making logs nicer to look at with some fancy components

---
+++ client/components/LogError.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { Log } from "@baby-stats/models/logs";
+
+  export let error: NonNullable<Log["error"]>;
+
+  $: stack = error?.stack;
+</script>
+
+<span class="log-error">
+  <span class="message">{error.message}</span>
+  <span class="stack">{stack}</span>
+</span>
+
+<style>
+  .log-error {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .log-error > .message {
+    color: var(--error-color-dark);
+    font-weight: bold;
+  }
+
+  .log-error > .stack {
+    white-space: pre;
+  }
+</style>
--- client/components/LogViewer.svelte
+++ client/components/LogViewer.svelte
@@ -1,26 +1,11 @@
-<script lang="ts" context="module">
-  const formatter = new Intl.DateTimeFormat("en-US", {
-    year: "2-digit",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  });
-</script>
-
 <script lang="ts">
   import { logs } from "../stores/logs";
+  import Log from "./Log.svelte";
 </script>
 
 <section class="log-viewer">
-  {#each $logs as { id, error, level, message, timestamp } (id)}
-    <p>
-      <span class="timestamp">{formatter.format(timestamp)}</span>
-      <span class={`level ${level}`}>[{level}]</span>
-      <span class="message">{message}</span>
-      <span class="stack">{error && error.stack ? error.stack : ""}</span>
-    </p>
+  {#each $logs as log (log.id)}
+    <Log {log} />
   {/each}
 </section>
 
@@ -28,46 +13,8 @@
   .log-viewer {
     max-height: 100%;
     width: 100%;
-    overflow: auto;
+    overflow-y: auto;
     border: var(--border-width) solid var(--border-color);
     border-radius: var(--border-radius);
-  }
-
-  .log-viewer p {
-    display: flex;
-    gap: 0.5rem;
-    font-size: 0.75rem;
-    line-height: 1rem;
-    margin: 0;
-    padding: 0.5rem;
-  }
-
-  .log-viewer p:nth-of-type(even) {
-    background-color: var(--button-color-hover);
-  }
-
-  .log-viewer span {
-    white-space: nowrap;
-  }
-
-  .log-viewer .level {
-    /* [error] is the largest at 7 characters */
-    min-width: 7ch;
-  }
-
-  .log-viewer .error {
-    color: var(--error-color);
-  }
-
-  .log-viewer .info {
-    color: var(--info-color);
-  }
-
-  .log-viewer .warn {
-    color: var(--warning-color);
-  }
-
-  .log-viewer .stack {
-    white-space: normal;
   }
 </style>
