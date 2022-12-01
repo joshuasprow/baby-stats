bfde9bea1ffde5da2b7d03ad1d78585646f1069a
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-30 20:28:59 -0500

    enabling multi-tab persistence

--- client/firebase/app.ts
+++ client/firebase/app.ts
@@ -1,7 +1,34 @@
+import { parseError } from "@baby-stats/lib/error";
 import { initializeApp as initializeFirebaseApp } from "@firebase/app";
 import { getAuth } from "@firebase/auth";
-import { enableIndexedDbPersistence, getFirestore } from "@firebase/firestore";
+import {
+  enableMultiTabIndexedDbPersistence,
+  enableIndexedDbPersistence,
+  getFirestore,
+  type Firestore,
+} from "@firebase/firestore";
+import logger from "../lib/logger";
 import type { FirebaseOptions } from "./options";
+
+const initializeFirestore = async (db: Firestore) => {
+  let enabled = false;
+
+  // TODO: not sure if this is necessary; i'm trying multitab persistence; if
+  try {
+    await enableMultiTabIndexedDbPersistence(db);
+    enabled = true;
+  } catch (error) {
+    logger.error(parseError(error));
+  }
+
+  if (enabled) return;
+  // that fails, i'll try single tab persistence
+  try {
+    await enableIndexedDbPersistence(db);
+  } catch (error) {
+    logger.error(parseError(error));
+  }
+};
 
 export const initializeApp = (options: FirebaseOptions) => {
   const app = initializeFirebaseApp(options);
@@ -9,7 +36,7 @@ export const initializeApp = (options: FirebaseOptions) => {
   const auth = getAuth(app);
   const db = getFirestore(app);
 
-  enableIndexedDbPersistence(db);
+  initializeFirestore(db);
 
   return { auth, db };
 };
