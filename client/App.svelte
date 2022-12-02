dd931c15132e16fc9ebf45ec8c773942567578a1
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-12-01 20:03:04 -0500

    making log options so that user store isn't being accessed before auth is initialized

--- client/App.svelte
+++ client/App.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
-  import GlobalError from "./components/GlobalError.svelte";
+  import { parseError } from "@baby-stats/lib/error";
+  import GlobalError, { setGlobalError } from "./components/GlobalError.svelte";
   import Layout from "./components/Layout/Layout.svelte";
   import ReloadPrompt from "./components/ReloadPrompt.svelte";
-  import "./main.css";
+  import logger from "./lib/logger";
   import { baby } from "./stores/baby";
   import { days } from "./stores/days";
   import { user } from "./stores/user";
+
+  import "./main.css";
   import "./variables.css";
+
+  window.onunhandledrejection = (e) => {
+    console.dir(e);
+
+    const error = parseError(e.reason);
+
+    setGlobalError(error);
+    logger.error(error);
+  };
 </script>
 
 <svelte:head>
