0a51d202aa8b791f8b434f4ab389941a09998940
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-12-31 21:31:06 -0500

    getting timestamp from id of *some* entries

--- scripts/tadpoles.ts
+++ scripts/tadpoles.ts
@@ -1,49 +1,31 @@
-import { z } from "zod";
-import data from "./tmp/tadpoles-data.json" assert { type: "json" };
 import { Entry } from "@baby-stats/models/entries";
-import { Timestamp } from "@firebase/firestore";
-import { EntryKind, EntryKindEnum } from "@baby-stats/models/entries-base";
+import { EntryKindEnum } from "@baby-stats/models/entries-base";
 import { Entry as TpEntry } from "@baby-stats/models/tadpoles";
+import { Timestamp } from "@firebase/firestore";
+import { parseError } from "./error";
+import data from "./tmp/tadpoles-data.json" assert { type: "json" };
 
-// const TpEntryType = z.enum([
-//   "bathroom",
-//   "nap",
-//   "note",
-//   "food",
-//   "activity",
-//   "skin_application",
-//   "medication",
-//   "health",
-// ]);
-// type TpEntryType = z.infer<typeof TpEntryType>;
+const logger = {
+  error: (error: unknown, extra = undefined as any) => {
+    const e = parseError(error);
 
-// const TpEntry = z.object({
-//   parent: z.boolean().optional(),
-//   start_time: z.number().optional(),
-//   end_time: z.number().optional(),
-//   type: TpEntryType,
-//   bathroom_type: z.literal("diaper").optional(),
-//   classification: z
-//     .string()
-//     .optional()
-//     .transform((s) =>
-//       s ? s.split(", ").map((c) => c.toLowerCase()) : undefined
-//     ),
-//   measure: z.enum(["oz", "F"]).optional(),
-//   amount_offered: z.number().optional(),
-//   quantity: z.string().or(z.number()).optional(),
-// });
-// type TpEntry = z.infer<typeof TpEntry>;
+    console.error(`\x1b[31mError:\x1b[0m ${e.message}`);
+
+    if (extra) console.error(extra);
+  },
+};
 
 const parseBathroomTpType = (classification: TpEntry["classification"]) => {
   if (!classification) {
     throw new Error("No classification for bathroom entry");
   }
 
-  if (classification.includes("dry")) return null;
+  const lowercase = classification.toLowerCase();
 
-  const isPee = classification.includes("wet");
-  const isPoop = classification.includes("bm");
+  if (lowercase.includes("dry")) return null;
+
+  const isPee = lowercase.includes("wet");
+  const isPoop = lowercase.includes("bm");
 
   if (isPee && isPoop) {
     return { pees: EntryKindEnum.Enum.pees, poops: EntryKindEnum.Enum.poops };
@@ -52,9 +34,7 @@ const parseBathroomTpType = (classification: TpEntry["classification"]) => {
   if (isPee) return EntryKindEnum.Enum.pees;
   if (isPoop) return EntryKindEnum.Enum.poops;
 
-  throw new Error(
-    `Unknown classification for bathroom entry: ${classification}`
-  );
+  throw new Error(`Unknown classification for bathroom entry: ${lowercase}`);
 };
 
 const parseTpType = ({
@@ -75,6 +55,24 @@ const parseTpType = ({
   }
 };
 
+const parseStartTime = ({
+  id,
+  start_time,
+}: Pick<TpEntry, "id" | "start_time">) => {
+  if (start_time) return Timestamp.fromMillis(start_time);
+
+  if (!id) {
+    console.error({ id, start_time });
+    throw new Error("No id or start time");
+  }
+
+  const millis = parseInt(id, 10);
+
+  if (isNaN(millis)) throw new Error(`Cannot convert id "${id}" to millis`);
+
+  return Timestamp.fromMillis(millis);
+};
+
 const parseTpEntry = (
   tpEntry: TpEntry
 ): Partial<Entry> | Partial<Entry>[] | null => {
@@ -82,11 +80,7 @@ const parseTpEntry = (
 
   if (!kinds) return null;
 
-  const { start_time } = tpEntry;
-
-  if (!start_time) throw new Error("No start time");
-
-  const timestamp = Timestamp.fromMillis(start_time);
+  const timestamp = parseStartTime(tpEntry);
 
   if (typeof kinds === "string") {
     const entry: Partial<Entry> = {
@@ -107,29 +101,25 @@ outer: for (const event of data.events) {
   if (!event.entries) continue;
 
   for (const entry of event.entries) {
-    try {
-      const tpEntry = TpEntry.parse({
-        parent: entry.parent,
-        start_time: entry.start_time,
-        end_time: entry.end_time,
-        type: entry.type,
-        bathroom_type: entry.bathroom_type,
-        classification: entry.classification,
-        measure: entry.measure,
-        amount_offered: entry.amount_offered,
-        quantity: entry.quantity,
-      });
+    const tpEntry = TpEntry.parse({
+      id: entry.id,
+      parent: entry.parent,
+      start_time: entry.start_time,
+      end_time: entry.end_time,
+      type: entry.type,
+      bathroom_type: entry.bathroom_type,
+      classification: entry.classification,
+      measure: entry.measure,
+      amount_offered: entry.amount_offered,
+      quantity: entry.quantity,
+    });
 
+    try {
       if (tpEntry.parent) continue;
 
       parseTpEntry(tpEntry);
     } catch (e) {
-      if (e instanceof z.ZodError) {
-        console.error(e.message);
-        console.error(entry);
-      } else {
-        throw e;
-      }
+      logger.error(e, tpEntry);
       break outer;
     }
   }
